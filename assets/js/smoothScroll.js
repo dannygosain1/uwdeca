dd8638bc649c6a8ddbeb03751aaae6040c6817fd
Danny Gosain <danny.gosain@autodesk.com>
2017-08-24 10:25:43 -0400

    added smooth scrolling on sections

---
+++ assets/js/smoothScroll.js
@@ -0,0 +1,23 @@
+$("#aboutHead").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+
+$("#teamHead").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#team").offset().top
+    }, 2000);
+});
+
+$("#resourcesHead").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#resources").offset().top
+    }, 3000);
+});
+
+$("#contactHead").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 3500);
+});
